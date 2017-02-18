ae33b0e3b17cca102e17cdb354399a72b6b12478
Ellie <delfieq@bitbucket.org>
2017-02-18 09:58:39 +0200

    Define a error message

--- Python/game.py
+++ Python/game.py
@@ -5,3 +5,8 @@ import random
 
 computer_choice = random.randint(1, 100)
 print('Намислих си едно число от 1 до 100. Опитай се да го познаеш.)')
+
+attempts = 1
+
+def error():
+    print("Грешка. Въведете валидно число.")
