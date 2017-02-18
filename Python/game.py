4d283cb2791bc83962a19dd157c31d2829d7e07c
Ellie <delfieq@bitbucket.org>
2017-02-18 10:45:16 +0200

    Check user input. Handle with the exceptions.

--- Python/game.py
+++ Python/game.py
@@ -18,3 +18,25 @@ def countAttemptsMsg():
 
 while (attempts <= 5):
     user_input = input("Въведете вашето предположение: ")
+
+    try:
+        user_num = int(user_input)
+
+        if (user_num <= 0):
+            print("Грешка. Числото не може да бъде ", user_num)
+            attempts -= 1
+        else:
+            if user_num == computer_choice:
+                print("Браво. Ти позна числото: ", user_num)
+                quit(True)
+            elif user_num < computer_choice:
+                print("Нагоре")
+            elif user_num > computer_choice:
+                print("Надолу")
+            else:
+                print("Упс. Нещо се обърка.")
+                quit(False)
+
+    except ValueError:
+        error()
+        attempts -= 1
